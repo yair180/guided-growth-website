2dd241be7861a12feaaeec8ef5541c705029e40e
yair180 <yair@thefearless.life>
2026-06-23 11:11:11 +0300

    Flow builder: auth-first default (sign up + age), Auth group

---
+++ onboarding-flow/assets/index-Dfc0VaJR.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Y1uFDMaR.js","assets/index-D7lp6Arw.js","assets/index-CrjThRhv.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-D7lp6Arw.js";const n=r("Preferences",{web:()=>t(()=>import("./web-Y1uFDMaR.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
---
+++ onboarding-flow/assets/index-DtG48xfV.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DI0PngAC.js","assets/index-D7lp6Arw.js","assets/index-CrjThRhv.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-D7lp6Arw.js";const _=e("Browser",{web:()=>o(()=>import("./web-DI0PngAC.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
