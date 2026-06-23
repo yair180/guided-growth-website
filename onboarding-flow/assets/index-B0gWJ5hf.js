cb16e693a2b4119f9d904c8e09cf150b9c499e58
yair180 <yair@thefearless.life>
2026-06-23 11:17:08 +0300

    Flow builder: split/fork with lanes (add split, edit lanes, drag components into lanes), export with branches

---
+++ onboarding-flow/assets/index-B0gWJ5hf.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C8g1WPpt.js","assets/index-xhtXnUkm.js","assets/index-SY_7ufQE.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-xhtXnUkm.js";const _=p("App",{web:()=>r(()=>import("./web-C8g1WPpt.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
---
+++ onboarding-flow/assets/index-DKPITb0c.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BnhfVMsH.js","assets/index-xhtXnUkm.js","assets/index-SY_7ufQE.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-xhtXnUkm.js";const n=r("Preferences",{web:()=>t(()=>import("./web-BnhfVMsH.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
