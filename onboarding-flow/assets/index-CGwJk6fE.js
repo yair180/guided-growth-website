ad56412bf6701f5435a5251af01eb65a503e8eb4
yair180 <yair@thefearless.life>
2026-06-24 21:07:07 +0300

    Flow builder: square green habit completion marks

---
+++ onboarding-flow/assets/index-CGwJk6fE.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BD2HHhzz.js","assets/index-BkrGYwID.js","assets/index-30JS4_EJ.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-BkrGYwID.js";const n=r("Preferences",{web:()=>t(()=>import("./web-BD2HHhzz.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
