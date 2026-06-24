9e50263de125bb19262802f144257d69052109d4
yair180 <yair@thefearless.life>
2026-06-24 20:58:33 +0300

    Flow builder: deeper beat editing + sync

---
+++ onboarding-flow/assets/index-D8_jT4EY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-YXjmcWaW.js","assets/index-DIAypjGc.js","assets/index-Dmjz78I3.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-DIAypjGc.js";const n=r("Preferences",{web:()=>t(()=>import("./web-YXjmcWaW.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
--- onboarding-flow/assets/index-DwGPDbwX.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CKbDqFRT.js","assets/index-celtGcLM.js","assets/index-Dmjz78I3.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-celtGcLM.js";const n=r("Preferences",{web:()=>t(()=>import("./web-CKbDqFRT.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
