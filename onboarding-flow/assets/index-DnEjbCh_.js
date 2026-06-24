17a19e2bef7a71d4cd70ec1b146fd91ca1b577bc
yair180 <yair@thefearless.life>
2026-06-25 00:35:08 +0300

    auto-mirror: 2026-06-25 00:35:05

---
+++ onboarding-flow/assets/index-DnEjbCh_.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-v4nDj9bv.js","assets/index-b5EG9RrV.js","assets/index-Ck60KZls.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-b5EG9RrV.js";const n=r("Preferences",{web:()=>t(()=>import("./web-v4nDj9bv.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
