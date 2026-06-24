aadc0dfb48c12053602c3cade5b8e456cbbbd15c
yair180 <yair@thefearless.life>
2026-06-24 19:09:16 +0300

    Flow builder: curated component palette + coach-line sync

---
+++ onboarding-flow/assets/index-BspwtsEY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-ceKasCz2.js","assets/index-8bqx-ZqA.js","assets/index-C9UtEn-S.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-8bqx-ZqA.js";const n=r("Preferences",{web:()=>t(()=>import("./web-ceKasCz2.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
