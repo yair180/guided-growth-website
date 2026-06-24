5476e4677dfcfcf3154e7142ce52e58f2f7afd23
yair180 <yair@thefearless.life>
2026-06-24 23:06:13 +0300

    Flow builder: phone-screen beats + orb + check-in menu bar

---
+++ onboarding-flow/assets/index-B0dHWSQ5.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Gx6SSfPC.js","assets/index-Boum7vFO.js","assets/index-DtqKKFZP.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-Boum7vFO.js";const n=r("Preferences",{web:()=>t(()=>import("./web-Gx6SSfPC.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
