529eb67dc1bd9c9df3c75733623fc4449caaddc0
yair180 <yair@thefearless.life>
2026-06-24 23:35:38 +0300

    Flow builder: per-beat background (warm chat) setting

---
+++ onboarding-flow/assets/index-B03w000Q.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D8ATB_54.js","assets/index-YUb0SQFw.js","assets/index-DHKNA6xP.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-YUb0SQFw.js";const n=r("Preferences",{web:()=>t(()=>import("./web-D8ATB_54.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
--- onboarding-flow/assets/index-BZ420E4U.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BwXZgAqz.js","assets/index-CQ00S5nE.js","assets/index-D19a2cel.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-CQ00S5nE.js";const n=r("Preferences",{web:()=>t(()=>import("./web-BwXZgAqz.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
