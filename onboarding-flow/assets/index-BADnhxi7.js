0288f3527660e49810019609a7eb4ada80526f57
yair180 <yair@thefearless.life>
2026-06-25 02:46:06 +0300

    auto-mirror: 2026-06-25 02:46:04

---
+++ onboarding-flow/assets/index-BADnhxi7.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CYRX7B78.js","assets/index-DlGB0cu0.js","assets/index-q9jJEwKl.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-DlGB0cu0.js";const n=r("Preferences",{web:()=>t(()=>import("./web-CYRX7B78.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
--- onboarding-flow/assets/index-CTHwu_iN.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C_udfR2g.js","assets/index-BnbNS-JM.js","assets/index-q9jJEwKl.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-BnbNS-JM.js";const n=r("Preferences",{web:()=>t(()=>import("./web-C_udfR2g.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
