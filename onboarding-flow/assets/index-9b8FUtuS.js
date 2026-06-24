cd8fb9c8c0f90a8aee40095ca333286dd4d315d6
yair180 <yair@thefearless.life>
2026-06-24 21:40:19 +0300

    Flow builder: multi-flow toggle (onboarding + morning/evening check-in)

--- onboarding-flow/assets/index-9b8FUtuS.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C3xlfWON.js","assets/index-BJ3oD8g5.js","assets/index-Dzcr5_Wn.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-BJ3oD8g5.js";const n=r("Preferences",{web:()=>t(()=>import("./web-C3xlfWON.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
