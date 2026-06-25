f4f1b416b61a707e259c46b20e115a22ec6ef098
yair180 <yair@thefearless.life>
2026-06-25 12:38:41 +0300

    auto-mirror: 2026-06-25 12:38:35

---
+++ onboarding-flow/assets/index-Cwq_a4lk.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BnwYq1o4.js","assets/index-DV__szDg.js","assets/index-DSiThrDX.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-DV__szDg.js";const n=r("Preferences",{web:()=>t(()=>import("./web-BnwYq1o4.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
--- onboarding-flow/assets/index-r2dCVb2E.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B6DRyXyJ.js","assets/index-DayqZJPq.js","assets/index-DSiThrDX.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-DayqZJPq.js";const n=r("Preferences",{web:()=>t(()=>import("./web-B6DRyXyJ.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
